b83c6a3d243bd6ed4d9bcccfb149c4166d4f7208
replakcan <alper.makcan@gmail.com>
2024-09-10 01:16:01 +0300

    Adds disabled prop to SiparisFormu_ekMalzemeler

--- src/components/SiparisFormu_ekMalzemeler.jsx
+++ src/components/SiparisFormu_ekMalzemeler.jsx
@@ -1,17 +1,17 @@
-export default function EkMalzemeler({handleInputChange, malzeme, checked}) {
-    return (
-      <>
-        <label className="malzeme">
-              <input
-                type="checkbox"
-                name="ek-malzeme"
-                value={malzeme}
-                onChange={handleInputChange}
-                checked={checked}
-              />{" "}
-              {malzeme}
-            </label>
-      </>
-    );
-  }
-  
+export default function EkMalzemeler({ disabled, handleInputChange, malzeme, checked }) {
+  return (
+    <>
+      <label className="malzeme">
+        <input
+          disabled={disabled}
+          type="checkbox"
+          name="ek-malzeme"
+          value={malzeme}
+          onChange={handleInputChange}
+          checked={checked}
+        />{" "}
+        {malzeme}
+      </label>
+    </>
+  );
+}
