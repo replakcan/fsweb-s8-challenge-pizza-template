8f30d128c311ed365549b51a6980b7cda533ad7e
replakcan <alper.makcan@gmail.com>
2024-09-10 00:59:36 +0300

    Formats document

--- src/components/SiparisFormu_siparisNotu.jsx
+++ src/components/SiparisFormu_siparisNotu.jsx
@@ -1,17 +1,16 @@
-export default function SiparisNotu({handleInputChange, siparisnotu}) {
-    return (
-      <>
-        <h3>Sipariş Notu</h3>
-        <label className="bold-label">
-          <textarea
+export default function SiparisNotu({ handleInputChange, siparisnotu }) {
+  return (
+    <>
+      <h3>Sipariş Notu</h3>
+      <label className="bold-label">
+        <textarea
           placeholder="Siparişine eklemek istediğin bir not var mı?"
-            id="siparis-notu"
-            name="siparis-notu"
-            value={siparisnotu}
-            onChange={handleInputChange}
-          />
-        </label>
-      </>
-    );
-  }
-  
+          id="siparis-notu"
+          name="siparis-notu"
+          value={siparisnotu}
+          onChange={handleInputChange}
+        />
+      </label>
+    </>
+  );
+}
