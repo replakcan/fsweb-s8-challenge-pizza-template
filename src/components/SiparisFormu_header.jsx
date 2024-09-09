31a746050db94dd65c34a9e3aa2334f001050f7a
replakcan <alper.makcan@gmail.com>
2024-09-09 15:07:04 +0300

    Add siparisformu componentfiles inside components

---
+++ src/components/SiparisFormu_header.jsx
@@ -0,0 +1,9 @@
+function SiparisFormuHeader() {
+  return (
+    <div className="giris-icerik">
+        <img src="Assets/Iteration-1-assets/logo.svg" />
+    </div>
+  );
+}
+
+export default SiparisFormuHeader;
