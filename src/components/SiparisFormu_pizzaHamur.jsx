067193ffad87d6b31c7e2fd1c165e7d6e8684466
replakcan <alper.makcan@gmail.com>
2024-09-09 15:49:30 +0300

    Update SiparisFormu_pizzaHamur component

---
+++ src/components/SiparisFormu_pizzaHamur.jsx
@@ -0,0 +1,18 @@
+export default function PizzaHamur({handleInputChange, hamur}) {
+    return (
+      <>
+        <label className="bold-label">
+            <select
+              value={hamur}
+              name="hamur"
+              onChange={handleInputChange}
+            >
+              <option value="İnce">İnce Hamur</option>
+              <option value="Orta">Orta Hamur</option>
+              <option value="Kalın">Kalın Hamur</option>
+            </select>
+          </label>
+      </>
+    );
+  }
+  
