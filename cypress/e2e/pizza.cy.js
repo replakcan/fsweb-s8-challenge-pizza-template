00ebc2dc27a3db9369ddfd721488c93f1c60c67d
replakcan <alper.makcan@gmail.com>
2024-09-10 19:07:22 +0300

    Updates cypress tests

--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -3,13 +3,35 @@ describe("siparis-formu", () => {
     cy.visit("http://localhost:5173/siparis-formu");
   });
 
+  // input alanini doldurur
   it(".type() - type into a DOM element", () => {
     cy.get("#text").type("Alper Mutlu AKCAN");
     cy.get("#text").should("have.value", "Alper Mutlu AKCAN");
   });
+
+  //ilgili checkboxlari isaretler, isaretlenmemesi gerekenler disabled mi diye bakar
   it(".check() - check a checkbox", () => {
     cy.get(".data-cy-not-disabled").not("[disabled]").check();
     cy.get(".data-cy-not-disabled").not("[disabled]").should("be.checked");
     cy.get(".data-cy-disabled").should("be.disabled");
   });
+
+  //tests for submit button
+  it(".click()", () => {
+    cy.get('select').select('Orta')
+
+    cy.get('.data-cy-boyut').check()
+
+    cy.get(".data-cy-not-disabled").not("[disabled]").check();
+    cy.get(".data-cy-not-disabled").not("[disabled]").should("be.checked");
+
+    cy.get("#text").type("Alper Mutlu AKCAN");
+    cy.get("#text").should("have.value", "Alper Mutlu AKCAN");
+
+    cy.get('.submit-btn').click()
+    cy.focused().click()
+    cy.contains('SİPARİŞ VER').click()
+  })
+
+
 });
