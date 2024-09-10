fb512cf153e47b1d6e1c6452373eaffeac139b0e
replakcan <alper.makcan@gmail.com>
2024-09-10 15:42:10 +0300

    Adds cypress test for checkbox elements

--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -1,23 +1,19 @@
-//inputa metin giren test
+describe("siparis-formu", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/siparis-formu");
+  });
 
-//birden fazla malzeme seçebilen bir test
+  it(".type() - type into a DOM element", () => {
+    cy.get("#text").type("Alper Mutlu AKCAN");
+    cy.get("#text").should("have.value", "Alper Mutlu AKCAN");
+  });
+  it(".check() - check a checkbox", () => {
+    cy.get('.data-cy-not-disabled').not("[disabled]").check();
 
-//formu gönderen bir test
+    cy.get('.data-cy-not-disabled').not("[disabled]").should("be.checked");
 
-describe('siparis-formu', () => {
-    beforeEach(() => {
-      // Cypress starts out with a blank slate for each test
-      // so we must tell it to visit our website with the `cy.visit()` command.
-      // Since we want to visit the same URL at the start of all our tests,
-      // we include it in our beforeEach function so that it runs before each test
-      cy.visit('http://localhost:5173/siparis-formu')
-    })
+    cy.get('.data-cy-disabled').not("[disabled]").check();
 
-    it('.type() - type into a DOM element', () => {
-        cy.get('#text').type('Alper Mutlu AKCAN')
-        cy.get('#text').should('have.value', 'Alper Mutlu AKCAN')
-
-
-
-})
-})
+    cy.get('.data-cy-disabled').not("[disabled]").should("be.disabled");
+  });
+});
