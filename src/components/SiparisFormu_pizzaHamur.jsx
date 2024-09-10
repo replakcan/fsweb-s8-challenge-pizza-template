7151604d1b4676a5d8fd466b14f6c78bbda9910a
replakcan <alper.makcan@gmail.com>
2024-09-10 18:51:42 +0300

    Adds a choose one option

--- src/components/SiparisFormu_pizzaHamur.jsx
+++ src/components/SiparisFormu_pizzaHamur.jsx
@@ -1,12 +1,13 @@
 export default function PizzaHamur({handleInputChange, hamur}) {
     return (
       <>
-        <label className="bold-label">
+        <label className="data-cy-selectbox">
             <select
               value={hamur}
               name="hamur"
               onChange={handleInputChange}
             >
+              <option selected disabled> Hamur tipi seçiniz</option>
               <option className="data-cy-hamur" value="İnce">İnce Hamur</option>
               <option value="Orta">Orta Hamur</option>
               <option value="Kalın">Kalın Hamur</option>
