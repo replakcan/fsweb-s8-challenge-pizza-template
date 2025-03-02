4ec6296ad5f7a5e45da513fe168337c4ccf94626
replakcan <alper.makcan@gmail.com>
2025-03-03 01:34:31 +0300

    Creates db files for repetitive html blocks

---
+++ db/meal-item.js
@@ -0,0 +1,32 @@
+export const mealItems = [
+  {
+    id: 0,
+    img: '1',
+    name: 'Noodle',
+  },
+  {
+    id: 1,
+    img: '2',
+    name: 'Pizza',
+  },
+  {
+    id: 2,
+    img: '3',
+    name: 'Hamburger',
+  },
+  {
+    id: 3,
+    img: '4',
+    name: 'French fries',
+  },
+  {
+    id: 4,
+    img: '5',
+    name: 'Fasty menu',
+  },
+  {
+    id: 5,
+    img: '6',
+    name: 'Coldies',
+  },
+]
---
+++ db/restaurant-menu.js
@@ -0,0 +1,26 @@
+export const restaurantMenu = [
+  {
+    id: 'id_1',
+    img: 1,
+    name: 'Meeeh Pizza-ish',
+    rating: 3.2,
+    soldCount: 123,
+    price: 235,
+  },
+  {
+    id: 'id_2',
+    img: 2,
+    name: 'Amedeus Mozarella Pizza',
+    rating: 4.4,
+    soldCount: 200,
+    price: 325,
+  },
+  {
+    id: 'id_3',
+    img: 3,
+    name: 'Dubai Hamburger',
+    rating: 4.9,
+    soldCount: 152,
+    price: 515,
+  },
+]
