e5a5c8f3b5b1d1e3275342fcdb6e9f0a9d49ec1b
replakcan <alper.makcan@gmail.com>
2025-03-03 13:38:33 +0300

    Updates footer communication texts

--- db/footer-communication-infos.js
+++ db/footer-communication-infos.js
@@ -2,16 +2,16 @@ export const communicationInfos = [
   {
     _id: 1,
     img: 1,
-    text: '341 Londonderry Road, Istanbul Türkiye',
+    text: '341 Londonderry Road, Istanbul Turkey',
   },
   {
     _id: 2,
     img: 2,
-    text: 'aciktim@teknolojikyemekler.com',
+    text: 'alper.makcan@gmail.com',
   },
   {
     _id: 3,
     img: 3,
-    text: '+90 216 123 45 67',
+    text: '+90 999 999 99 99',
   },
 ]
